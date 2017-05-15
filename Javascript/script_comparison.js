944c62edc9b85166119cc64239749ff6ba97951e
KhooBaoXuan <ramousstyle@gmail.com>
2017-05-16 01:29:12 +0800

    minor changes

--- Javascript/script_comparison.js
+++ Javascript/script_comparison.js
@@ -33,15 +33,3 @@ $(".scroll-box6").click(function scrollToFull() {
         scrollTop: $("#full").offset().top},
         'slow');
 });
-
-//hide window based on window size
-$(window).on("orientationchange load resize", function () {
-     var width = $(document).width();
-     if(width<1765){
-           $("#sticky").hide();
-          //  code for other elements hide stuff
-     }
-     else if(width>765){
-          // for show iamges
-     }
-});
