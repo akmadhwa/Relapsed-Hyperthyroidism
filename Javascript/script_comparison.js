20626d3ef738445613c1c11cf54ae5ff20bd10fd
KhooBaoXuan <ramousstyle@gmail.com>
2017-05-15 23:05:32 +0800

    comparison script and style

--- Javascript/script_comparison.js
+++ Javascript/script_comparison.js
@@ -33,3 +33,29 @@ $(".scroll-box6").click(function scrollToFull() {
         scrollTop: $("#full").offset().top},
         'slow');
 });
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the n
