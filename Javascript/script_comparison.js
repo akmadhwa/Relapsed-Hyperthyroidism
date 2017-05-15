c69493609951e0af16f70a3940e878a6242f649f
KhooBaoXuan <ramousstyle@gmail.com>
2017-05-16 01:12:19 +0800

    change some color

--- Javascript/script_comparison.js
+++ Javascript/script_comparison.js
@@ -34,28 +34,14 @@ $(".scroll-box6").click(function scrollToFull() {
         'slow');
 });
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
+//hide window based on window size
+$(window).on("orientationchange load resize", function () {
+     var width = $(document).width();
+     if(width<1765){
+           $("#sticky").hide();
+          //  code for other elements hide stuff
+     }
+     else if(width>765){
+          // for show iamges
+     }
 });
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the n
